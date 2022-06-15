6fccb74d0df6ed9334e07954eb17ceb03d339059
SvetlanaGracheva <SvetlanaGracheva>
2022-06-15 19:22:35 +0300

    new email

--- src/test/java/org/tms/util/Constants.java
+++ src/test/java/org/tms/util/Constants.java
@@ -2,14 +2,14 @@ package org.tms.util;
 
 public class Constants {
     public static final String LOGIN_PAGE_URL = "http://prestashop.qatestlab.com.ua/ru/authentication?back=my-account";
-    public static final String EMAIL = "ivanov7@mailto.plus";
-    public static final String PASSWORD = "qwe123";
-    public static final String EMAIL_CREATE = "ivanov7@mailto.plus";
+    public static final String EMAIL = "ivanov0707@mailto.plus";
+    public static final String PASSWORD = "qwert12345";
+    public static final String EMAIL_CREATE = "ivanov0707@mailto.plus";
     public static final String EXPECTED_TEXT_OF_NAME_OF_MY_ACCOUNT_PAGE = "Welcome to your account. Here you can manage all of your personal information and orders.";
     public static final String EXPECTED_TEXT_AUTHENTICATION_FAILED= "There is 1 error";
     public static final String FIRST_NAME= "Ivanov";
     public static final String LAST_NAME ="Ivan";
-    public static final String PASSWORD_CREATE = "qwe123";
+    public static final String PASSWORD_CREATE = "qwert12345";
     public static final String PAGE_URL_ADDRESS ="http://prestashop.qatestlab.com.ua/ru/addresses";
     public static final String EXPECTED_MESSAGE_OF_CHECKOUT_PAGE ="Ваш заказ http://prestashop.qatestlab.com.ua/ выполнен.";
 
